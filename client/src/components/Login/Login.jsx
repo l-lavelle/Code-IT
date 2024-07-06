6ece97982988ac47134618a2380c2b199ddd6281
Lauren <lauren.m.lavelle@wmich.edu>
2024-07-05 20:08:51 -0400

    cleaned login and signup code

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -4,22 +4,11 @@ import React, { useState } from "react";
 import '../../Variables.css'
 import './Login.css'
 import Form from 'react-bootstrap/Form';
-import CodeCat from '../CodeCat/CodeCat';
-import Signup from '../Signup/Signup';
+
 
 const Login = () => {
-  // const [switched, setSwitched] = useState(false);
-  // const [signupData, setSignupData] = useState({ username: '', password: '' });
   const [loginData, setLoginData] = useState({ username: '', password: '' , confirmPassword:''});
-  // const [username, setUsername] = useState('');
-  // const [password, setPassword] = useState('');
-  // const [confirmPassword, setConfirmPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-
-  // const updateSignup= async (event)=>{
-  //   const { name, value } = event.target;
-  //   setSignupData({ ...signupData, [name]: value });
-  // };
 
   const updateLogin= async (event)=>{
     const { name, value } = event.target;
@@ -78,72 +67,6 @@ const Login = () => {
    </button>
   </Form>
   </>
-
-//  <main className="SL-wrapper">
-// { switched===true? <h2 className="pt-5">SignUp</h2>:<h2 className="pt-5">Login</h2> }
-//   <div className="p-3 signup-login-container">
-//     <div className={switched===true? "hidden ":"visible login-container"}>
-//      <Form onSubmit={handleLogin}>
-//       <Form.Group className="mb-3 text-left signup-login-text">
-//         <Form.Label>Username</Form.Label>
-//         <Form.Control 
-//         type="text" 
-//         name="username"
-//         value={loginData.username}
-//         onChange={updateLogin}
-//         />
-//       </Form.Group>
-
-//       <Form.Group className="mb-3 text-left signup-login-text">
-//         <Form.Label>Password</Form.Label>
-//         <Form.Control 
-//          type="password" 
-//          name="password"
-//          value={loginData.password}
-//          onChange={updateLogin}/>
-//       </Form.Group>
-//      <button className="button-ct" variant="primary" type="submit">
-//           Submit
-//      </button>
-//     </Form>
-//     </div>
-//     <div className={switched===true? "hidden":"visible signup-switch-container"}>
-//      <p>
-//             Don't have an account yet? <span className="SL-switch-text" onClick={()=>setSwitched(true)}>Signup</span>
-//      </p>
-//     <CodeCat />
-//     </div>
-
-
-//   <div className={switched===true? "visible signup-container":"hidden"}>
-//         <Signup/> 
-//           <Form>
-//           <Form.Group className="mb-3 text-left signup-login-text" >
-//             <Form.Label>Email Address</Form.Label>
-//             <Form.Control type="text" />
-//           </Form.Group>
-
-//           <Form.Group className="mb-3 text-left signup-login-text">
-//             <Form.Label>Password</Form.Label>
-//             <Form.Control type="password" />
-//           </Form.Group>
-
-//           <Form.Group className="mb-3 text-left signup-login-text" >
-//               <Form.Label>Confirm Password</Form.Label>
-//               <Form.Control type="password" />
-//           </Form.Group>
-//         </Form>
-//         <button className="button-ct" variant="primary" type="submit">
-//              Submit
-//         </button>
-//         </div>
-//         <div className={switched===true? "visible login-switch-container":"hidden"}>
-//         <p> Already have an account? <span className="SL-switch-text" onClick={()=>setSwitched(false)}>Login</span>
-//          </p>
-//         <CodeCat /> 
-//       </div>
-//    </div>
-// </main> 
   );
 };
 
--- client/src/components/Signup/Signup.jsx
+++ client/src/components/Signup/Signup.jsx
@@ -2,16 +2,10 @@ import React, { useState } from "react";
 import '../../Variables.css'
 import '../Login/Login.css'
 import Form from 'react-bootstrap/Form';
-import CodeCat from '../CodeCat/CodeCat';
-import Login from '../Login/Login';
+
 
 const Signup = () => {
-  const [switched, setSwitched] = useState(false);
-  const [signupData, setSignupData] = useState({ username: '', password: '' });
-//   const [loginData, setLoginData] = useState({ username: '', password: '' , confirmPassword:''});
-  // const [username, setUsername] = useState('');
-  // const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [signupData, setSignupData] = useState({ username: '', password: '' , confirmPassword:''});
   const [errorMessage, setErrorMessage] = useState('');
 
   const updateSignup= async (event)=>{
@@ -19,14 +13,10 @@ const Signup = () => {
     setSignupData({ ...signupData, [name]: value });
   };
 
-//   const updateLogin= async (event)=>{
-//     const { name, value } = event.target;
-//     setLoginData({ ...loginData, [name]: value });
-//   };
 
-  const handleLogin = async (event) => {
+  const handleSignup = async (event) => {
     event.preventDefault();
-   
+//    Update to signup
     try {
       let domain = window.location.origin;
       var url = new URL(domain);
@@ -54,7 +44,7 @@ const Signup = () => {
     <>
       <Form>
           <Form.Group className="mb-3 text-left signup-login-text" >
-            <Form.Label>Email Address</Form.Label>
+            <Form.Label>Username</Form.Label>
             <Form.Control type="text" />
           </Form.Group>
 
@@ -67,43 +57,11 @@ const Signup = () => {
               <Form.Label>Confirm Password</Form.Label>
               <Form.Control type="password" />
           </Form.Group>
-        </Form>
         <button className="button-ct" variant="primary" type="submit">
         Submit
         </button>
+        </Form>
         </>
-//     <>
-//    <div className={switched===false? "visible signup-container":"hidden"}>
-//         <Login/>
-//     </div>
-//   <div className={switched===true? "visible signup-container":"hidden"}>
-
-//          <Form>
-//           <Form.Group className="mb-3 text-left signup-login-text" >
-//             <Form.Label>Email Address</Form.Label>
-//             <Form.Control type="text" />
-//           </Form.Group>
-
-//           <Form.Group className="mb-3 text-left signup-login-text">
-//             <Form.Label>Password</Form.Label>
-//             <Form.Control type="password" />
-//           </Form.Group>
-
-//           <Form.Group className="mb-3 text-left signup-login-text" >
-//               <Form.Label>Confirm Password</Form.Label>
-//               <Form.Control type="password" />
-//           </Form.Group>
-//         </Form>
-//         <button className="button-ct" variant="primary" type="submit">
-//              Submit
-//         </button>
-//         {/* </div> */}
-//         {/* <div className={switched===true? "visible login-switch-container":"hidden"}> */}
-//         <p> Already have an account? <span className="SL-switch-text" onClick={()=>setSwitched(false)}>Login</span>
-//          </p>
-//         <CodeCat />
-//       </div>
-//       </>
   );
 };
 
