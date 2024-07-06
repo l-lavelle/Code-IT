08983885db8bf03f93cc595b147a712f22376fe8
Lauren <lauren.m.lavelle@wmich.edu>
2024-07-06 09:08:30 -0400

    fixing production path heroku deploy test

--- server/server.js
+++ server/server.js
@@ -18,10 +18,10 @@ app.use(routes);
 
 // trial for heroku deployment
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
 }
 
