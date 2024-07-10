76ec9ff6645356dc9b0ff174f3cabb7599a7eab9
Lauren <lauren.m.lavelle@wmich.edu>
2024-07-10 19:34:47 -0400

    condense code questions homepage

--- client/src/components/QuestionsHomepage/QuestionsHomepage.jsx
+++ client/src/components/QuestionsHomepage/QuestionsHomepage.jsx
@@ -5,7 +5,8 @@ import './QuestionsHomepage.css'
 import QHDropdown from './QHDropdown';
 import Check from "../../assets/check.svg";
 import { useEffect, useState } from 'react';
-import {InputGroup, Form, DropdownButton, Dropdown} from 'react-bootstrap';
+import {InputGroup, Dropdown} from 'react-bootstrap';
+import {findURL} from '../../utils/general';
 // If logged in need to figure out if correct or not
 
 const QuestionsHomepage = () => {
@@ -16,14 +17,7 @@ const QuestionsHomepage = () => {
   const [difficulty, setDifficulty] = useState();
 
   useEffect(() => {
-    let url2
-    let domain = window.location.origin;
-    var url = new URL(domain);
-    url.port = '3001';  
-    url2 = `${url}question/${languageInfo.languageId}/${difficultyInfo.difficultyId}`;
-    if (process.env.NODE_ENV === "production") {
-        url2 = `${domain}/question/${languageInfo.languageId}/${difficultyInfo.difficultyId}`;
-    }
+    let url2 = findURL(`question/${languageInfo.languageId}/${difficultyInfo.difficultyId}`)
     fetch(url2, {
       method: 'GET',
       headers: { "Content-Type": "application/json" },
@@ -37,14 +31,7 @@ const QuestionsHomepage = () => {
   }, [difficultyInfo.difficultyId, languageInfo.languageId]);
 
   useEffect(() => {
-    let url2
-    let domain = window.location.origin;
-    var url = new URL(domain);
-    url.port = '3001';  
-    url2 = `${url}question/languages`;
-    if (process.env.NODE_ENV === "production") {
-        url2 = `${domain}/question/languages`;
-    }
+    let url2 = findURL(`question/languages`)
     fetch(url2, {
       method: 'GET',
       headers: { "Content-Type": "application/json" },
@@ -58,14 +45,7 @@ const QuestionsHomepage = () => {
   }, []);
 
   useEffect(() => {
-    let url2
-    let domain = window.location.origin;
-    var url = new URL(domain);
-    url.port = '3001';  
-    url2 = `${url}question/difficulty`;
-    if (process.env.NODE_ENV === "production") {
-        url2 = `${domain}/question/difficulty`;
-    }
+    let url2 = findURL(`question/difficulty`)
     fetch(url2, {
       method: 'GET',
       headers: { "Content-Type": "application/json" },
@@ -86,11 +66,12 @@ const QuestionsHomepage = () => {
   return (
     <div className="qh-body">
     <h2 className="pt-3">Coding Challenges</h2>
-    <p className="m-2">Ready to test your skills? Choose the lanuage you want to use and the difficulty level for the challenge questions. Click on the question in order to start the challenge.</p>
+    <p className="m-2 qh-summary">Ready to test your skills? Choose the lanuage you want to use and the difficulty level for the challenge questions. Click on the question in order to start the challenge.</p>
     <div className=" ms-3 me-3 ">
     <InputGroup className="mb-3">
        <QHDropdown 
        title={"Language"}
+       value={languageInfo.languageType}
        input= {
         languages.map((language) => (
           <>
@@ -103,69 +84,15 @@ const QuestionsHomepage = () => {
           </>
           ))
         }
-        value={languageInfo.languageType}
         />
-        </InputGroup>
-
-    {/* <InputGroup className="mb-3">
-        <DropdownButton
-          variant="outline-secondary"
-          title="Language"
-          id="input-group-dropdown-1"
-           className="highlight-lang"
-        >
-          {
-        languages.map((language) => (
-          <>
-              <Dropdown.Item 
-                className={language.language_type===languageInfo.languageType?"highlight-lang":null}
-                onClick={()=>setLanguageInfo({languageId:language.id, languageType:language.language_type})}>
-                {language.language_type}
-                </Dropdown.Item>
-              <Dropdown.Divider />
-          </>
-          ))
-        }
-        </DropdownButton>
-        <Form.Control 
-        readOnly
-        aria-label="Text input with dropdown button" 
-        value={languageInfo.languageType}
-        />
-      </InputGroup> */}
+    </InputGroup>
     </div>
 
     <div className=" ms-3 me-3 qh-difficulty-dd">
-    {/* <InputGroup className="mb-3">
-        <DropdownButton
-          variant="outline-secondary"
-          title="Difficulty"
-          id="input-group-dropdown-1"
-          className="highlight-lang"
-        >
-          {
-        difficulty.map((diff) => (
-          <>
-              <Dropdown.Item 
-                className={parseInt(difficultyInfo.difficultyId)===diff.id?"highlight-lang":null}
-                onClick={()=>setDifficultyInfo({difficultyId:diff.id, difficultyType:diff.difficulty_type})}>
-                {diff.difficulty_type}
-                </Dropdown.Item>
-              <Dropdown.Divider />
-          </>
-          ))
-        }
-        </DropdownButton>
-        <Form.Control 
-        readOnly
-        aria-label="Text input with dropdown button" 
-        value={difficultyInfo.difficultyType}
-        />
-      </InputGroup> */}
-
       <InputGroup className="mb-3">
        <QHDropdown 
        title={"Difficulty"}
+       value={difficultyInfo.difficultyType}
        input={
         difficulty.map((diff) => (
           <>
@@ -178,7 +105,6 @@ const QuestionsHomepage = () => {
           </>
           ))
         } 
-        value={difficultyInfo.difficultyType}
         />  
         </InputGroup>
     </div>
@@ -196,25 +122,24 @@ const QuestionsHomepage = () => {
     </div>
 
     <div className="qh-main-content me-3 ms-3 p-3 mb-3">
-    <ol className="ol-qh-questions ps-3">
-      {
-        questions.map((question) => (
-        <li key={question.id} className="pt-2 pb-2 ps-2 m-2 qh-questions"
-        onClick={()=>window.location.assign(`/challenge/${question.id}`)}>
-          {question.question}
-        </li>
+      <ol className="ol-qh-questions ps-3">
+        {
+          questions.map((question) => (
+          <li key={question.id} className="pt-2 pb-2 ps-2 m-2 qh-questions"
+          onClick={()=>window.location.assign(`/challenge/${question.id}`)}>
+            {question.question}
+          </li>
+          ))
+        }
+          {/* <li className="pt-2 pb-2 ps-2 m-2 qh-questions">
+            havent anserwered question
 
-        ))
-      }
-        <li className="pt-2 pb-2 ps-2 m-2 qh-questions">
-           havent anserwered question
-
-        </li>
-        <li className="pt-2 pb-2 ps-2 m-2  qh-questions-correct">
-             correct question
-             <img src={Check} className="ps-2" alt="Correct Question Checkmark"></img>
-        </li>
-        </ol>
+          </li>
+          <li className="pt-2 pb-2 ps-2 m-2  qh-questions-correct">
+              correct question
+              <img src={Check} className="ps-2" alt="Correct Question Checkmark"></img>
+          </li> */}
+      </ol>
     </div>
     </div>
   );
