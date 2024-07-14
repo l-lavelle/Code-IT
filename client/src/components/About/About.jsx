3f98ee8d197185dabf2bea93f8baa0f63171c7e3
Lauren <lauren.m.lavelle@wmich.edu>
2024-07-14 19:52:44 -0400

    fixing about responsiveness

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,4 +1,3 @@
-// TODO: add in all content, possible new dev picture if can find one
 import React from "react";
 import '../../Variables.css';
 import './About.css';
@@ -12,10 +11,10 @@ const About = () => {
     <div className="d-flex justify-content-center  about-wrapper">
         <div className="about-info mx-3 px-2 pb-2 pt-2 mb-3">
             <div className="about-title">
-            <h2 lg={4}><span id="spaceBreak">About </span><span >CodeIT</span></h2>
+            <h2 md={6} lg={4}><span id="spaceBreak">About </span><span >CodeIT</span></h2>
             <CodeCat/>
             </div>
-            <div lg={7} className="text-left">
+            <div md={6} lg={7} className="text-left">
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
             </div>
@@ -23,10 +22,10 @@ const About = () => {
     </div>
    
     <Row className="lo-info-container mx-3">
-        <Col md={5} lg={5} xl={4}>
+        <Col md={12} lg={5} xl={4}>
             <img id="lo-pic" src={laurenPic} alt="Site Creator"/>
         </Col>
-        <Col md={6} className="lo-info pt-2 pb-2">
+        <Col md={12} lg={7} className="lo-info pt-2 pb-2">
             <h4 >Meet the Dev</h4>
             <p>Hi, Nice to Meet You! orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
         </Col>
