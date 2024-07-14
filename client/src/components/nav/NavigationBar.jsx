29cc8c4f7203cc4ba7800a93c325bf0dd57ce78e
Lauren <lauren.m.lavelle@wmich.edu>
2024-07-14 19:23:53 -0400

    update nav bar remove default links

--- client/src/components/nav/NavigationBar.jsx
+++ client/src/components/nav/NavigationBar.jsx
@@ -1,4 +1,3 @@
-// TODO: Add icon to name
 import React from "react";
 import {Container, Navbar, Nav, NavDropdown} from 'react-bootstrap';
 import '../../Variables.css';
@@ -14,14 +13,9 @@ const NavigationBar = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" className="bg-white"/>
             <Navbar.Collapse id="basic-navbar-nav" >
               <Nav className="ms-auto">
-                {/* <Nav.Link href="/QuestionsHomepage">Questions Homepage</Nav.Link> */}
                 <NavDropdown title=" Start Coding" id="basic-nav-dropdown">
                   <NavDropdown.Item href="/QuestionsHomepage">Code Challenges</NavDropdown.Item>
                   <NavDropdown.Item href="/freeCode">Free Code</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                 </NavDropdown>
                 {AuthService.loggedIn()?(<Nav.Link onClick={AuthService.logout}>Logout</Nav.Link>):(<Nav.Link href="/Login">Login</Nav.Link>)}
               </Nav>
